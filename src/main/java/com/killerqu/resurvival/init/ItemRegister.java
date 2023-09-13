0edb47de39ba56db62c6661ab81f407898ee257f
Killerqu00 <killerqueen1777@gmail.com>
2023-09-13 16:03:44 +0200

    add firestarter kit

--- src/main/java/com/killerqu/resurvival/init/ItemRegister.java
+++ src/main/java/com/killerqu/resurvival/init/ItemRegister.java
@@ -1,6 +1,8 @@
 package com.killerqu.resurvival.init;
 
 import com.killerqu.resurvival.main.ReSurvival;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.item.FlintAndSteelItem;
 import net.minecraft.item.Item;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
@@ -8,8 +10,10 @@ import net.minecraft.util.Identifier;
 import org.slf4j.Logger;
 
 public class ItemRegister {
-    public static void register(String name, Item item) {
+    public static final Item FIRESTARTER_KIT = register("firestarter_kit", new FlintAndSteelItem(new FabricItemSettings().maxDamage(4)));
+    public static Item register(String name, Item item) {
         Registry.register(Registries.ITEM, new Identifier(ReSurvival.MODID, name), item);
+        return item;
     }
     public static void registerAll(Logger logger) {
         logger.info("Items registered!");
