8b6434583a557cd67c408ea5e8096c31ea259956
Killerqu00 <killerqueen1777@gmail.com>
2023-09-13 15:11:05 +0200

    init

---
+++ src/main/java/com/killerqu/resurvival/ReSurvival.java
@@ -0,0 +1,13 @@
+package com.killerqu.resurvival;
+
+import net.fabricmc.api.ModInitializer;
+
+public class ReSurvival implements ModInitializer {
+    /**
+     * Runs the mod initializer.
+     */
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ src/main/java/com/killerqu/resurvival/client/ReSurvivalClient.java
@@ -0,0 +1,13 @@
+package com.killerqu.resurvival.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ReSurvivalClient implements ClientModInitializer {
+    /**
+     * Runs the mod initializer on the client environment.
+     */
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
